d04747bb194546e73611cdd54d24cb8b5597c0e3
Davi Batista <davideosmar11@gmail.com>
2025-08-01 18:29:17 -0300

    feat: added env variable for the API and the user register service

--- src/app/api/auth/user/user-register/user-register.service.ts
+++ src/app/api/auth/user/user-register/user-register.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IAPIError } from '../../../../../server/utils/IAPIError';
+
+export interface IUserRegisterBody {
+  name: string;
+  email: string;
+  phone_number: string;
+  password: string;
+  password_confirmation: string;
+  birth_date: string;
+  check_privacy: boolean;
+  address: {
+    cep: string;
+    street: string;
+    number: string;
+    complement: string;
+    uf_id: string;
+    city: string;
+  },
+  gym_plan_id: string;
+}
+
+export interface IUserRegisterResponse {
+  token: string;
+  id: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserRegisterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  async userRegister (body: IUserRegisterBody): Promise<IUserRegisterResponse | IAPIError> {
+    const API_URL = process.env['API_URL']
+
+    const result = this.http.post<IUserRegisterResponse | IAPIError>(`${API_URL}/user/register`, body)
+
+    return new Promise((resolve, reject) => {
+      result.subscribe({
+        next: (response) => resolve(response),
+        error: (error) => reject(error)
+      });
+    } );
+
+  }
+
 }
---
+++ src/server/utils/IAPIError.ts
@@ -0,0 +1,6 @@
+export interface IAPIError {
+    message: string
+    statusCode: number
+    timestamp: string
+    path: string
+}
